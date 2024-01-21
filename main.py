be303a090ca2f56b9b9970c4687221ed8829b89b
UCMendes <Uche.mendes@gmail.com>
2024-01-21 15:07:11 +0000

    Can calc for >2 numbers in sequence (ex 2+2+2)

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def calc_result(term1, sign, term2):
 # set regex rules for determining a valid expression
 number = re.compile(r"\-?[0-9]{1,}")
 sign = re.compile(r"[\*\+\-\/]")
-expression = re.compile(number.pattern + rf"({sign.pattern}{number.pattern})*")
+exp = re.compile(number.pattern + rf"({sign.pattern}{number.pattern})*")
+bracketed_exp = re.compile(rf"\({exp.pattern}\)")
 
 # Get expression from user, make sure it's valid
 end = False
@@ -26,14 +27,12 @@ end = False
 while not end:
   valid_calc = False
   while not valid_calc:
-    calc_store = str(input("""
-                     Enter what you want to calculate, or enter 'X' to exit: 
-                          """))
+    calc_store = str(input("""Enter what you want to calculate, or enter 'X' to exit: """))
     if calc_store == "X":
       print("Thanks for using this calculator.")
       exit()
     calc_store = calc_store.replace(" ", "")
-    if re.fullmatch(expression, calc_store) == None:
+    if re.fullmatch(exp, calc_store) == None:
       print("""Invalid expression.
   There needs to exist at least one whole number, followed by a symbol, followed by another whole number.
   Example formats include "2 + 2" and "-4 / 2".
@@ -43,9 +42,18 @@ while not end:
 
   # Split calculation into components of number and sign
   calc_list = re.split(rf"({sign.pattern})", calc_store)
-  # Calculate and output answer
+  # Find initial answer
   answer = calc_result(calc_list[0], calc_list[1], calc_list[2])
+  # While we haven't seen every list element
+  count = 4
+  while count <= (len(calc_list) - 1):
+    # Add previous answer to next elements:
+    answer = calc_result(answer, calc_list[count - 1], calc_list[count])
+    count += 2
+  
+  # output answer
   print(f"Answer: {answer}")
+
 
 
 
